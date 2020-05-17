0012356d2a9bc6c055fab3bef86d4242cad3120a
Ablo224 <ablobalde92@gmail.com>
2020-05-17 13:12:30 +0200

    création de la table ensemble des formes, et celle liant les formes et l'ensemble des formes

--- src/main/java/uvsq21606235/bdd/BaseDeDonnees.java
+++ src/main/java/uvsq21606235/bdd/BaseDeDonnees.java
@@ -46,7 +46,7 @@ public abstract class BaseDeDonnees {
 		 * @throws SQLException
 		 */
 		
-		private static void createTableForme(final Connection conn)throws SQLException {
+		private static void createTableForme(Connection conn)throws SQLException {
 	        String table = "create table Formes ("
 	                + "nomForme varchar(20) not null primary key"
 	                + ")";
@@ -59,7 +59,7 @@ public abstract class BaseDeDonnees {
 		 * @param conn
 		 * @throws SQLException
 		 */
-		private static void creatTableCarre(final Connection conn)throws SQLException {
+		private static void creatTableCarre(Connection conn)throws SQLException {
 	        String laTable = "create table Carre ("
 	                + "nomForme varchar(20) not null primary key,"
 	                + "origine_x double,"
@@ -76,7 +76,7 @@ public abstract class BaseDeDonnees {
 		 * @param conn
 		 * @throws SQLException
 		 */
-		private static void creatTableCercle(final Connection conn)throws SQLException {
+		private static void creatTableCercle(Connection conn)throws SQLException {
 	        String laTable = "create table Cercle ("
 	                + "nomForme varchar(20) not null primary key,"
 	                + "centre_x double,"
@@ -94,7 +94,7 @@ public abstract class BaseDeDonnees {
 		 * @param conn
 		 * @throws SQLException
 		 */
-		private static void creatTableRectangle(final Connection conn)throws SQLException {
+		private static void creatTableRectangle(Connection conn)throws SQLException {
 	        String laTable = "create table Rectangle ("
 	                + "nomForme varchar(20) not null primary key,"
 	                + "origine_x double,"
@@ -112,7 +112,7 @@ public abstract class BaseDeDonnees {
 		 * @param conn
 		 * @throws SQLException
 		 */
-		private static void creatTableTriangle(final Connection conn)throws SQLException {
+		private static void creatTableTriangle(Connection conn) throws SQLException {
 	        String laTable = "create table Triangle ("
 	                + "nomForme varchar(20) not null primary key,"
 	                + "p1_x double,"
@@ -126,6 +126,41 @@ public abstract class BaseDeDonnees {
 	        Statement etat = conn.createStatement();
 	        etat.execute(laTable);
 	    }
+		
+		
+		/**
+		 * création de la table regroupe l'ensemble des formes
+		 * @param conn
+		 * @throws SQLException
+		 */
+		private static void creationTableEnsembleForme(Connection conn) throws SQLException {
+	        String laTable = "create table EnsembleForme ("
+	                + "nomGroupe varchar(20) not null primary key,"
+	                + "foreign key (nomGroupe) references Formes (nomF)"
+	                + ")";
+	        Statement etat = conn.createStatement();
+	        etat.execute(laTable);
+	    }
+		
+		
+		/**
+		 * création de la relation resultante de l'ensemble des formes 
+		 * et de l'abstraction des formes
+		 * @param conn
+		 * @throws SQLException
+		 */
+		private static void creationTableDeLien(Connection conn) throws SQLException {
+	        String laTable = "create table Relation ("
+	                + "idG varchar(20),"
+	                + "idC varchar(20),"
+	                + "primary key (idG, idC),"
+	                + "foreign key (idG) references EnsembleForme (nomGroupe),"
+	                + "foreign key (idC) references Formes (nomF)"
+	                + ")";
+	        Statement etat = conn.createStatement();
+	        etat.execute(laTable);
+	    }
+		
 		
 		
 		
